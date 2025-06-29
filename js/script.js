57d97aef9a5ab0efb84572b4c14bb5a6e20b2877
abhinav37kr <8848533148r@gmail.com>
2025-06-29 19:20:26 +0530

    final

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let currFolder;
 async function getSongs(folder) {
 
     currFolder = folder;
-    let a = await fetch(`https://github.com/abhinav-37kr/Musify-/tree/main/songs/${folder}/`);
+    let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
     let response = await a.text();
     console.log(response);
     let div = document.createElement("div");
@@ -70,7 +70,7 @@ const playMusic = (track, pause = false) => {
 }
 
 async function displayAlbums() {
-    let a = await fetch(`https://github.com/abhinav-37kr/Musify-/tree/main/songs`);
+    let a = await fetch(`http://127.0.0.1:5500/songs/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -86,7 +86,7 @@ async function displayAlbums() {
 
             let folder = e.href.split("/").slice(-1)[0];
             //get metadata of folder
-            let a = await fetch(`https://github.com/abhinav-37kr/Musify-/tree/main/songs/${folder}/info.json`);
+            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
             let response = await a.json();
 
             cardContainer.innerHTML += `<div data-folder="${folder}" class="card">
